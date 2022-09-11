9ece13fdb51f61555895f7ca3401825c90350be1
Andry Zolalaina <alphabitic@gmail.com>
2022-09-11 09:10:37 +0200

    make it better

--- server.js
+++ server.js
@@ -10,15 +10,6 @@ const cors = require('cors');
 // ... other imports 
 const path = require("path")
 
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
-
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
 const app = express();
 const corsOptions = {
   origin: process.env.CLIENT_URL,
